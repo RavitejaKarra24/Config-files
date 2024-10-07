e3c9a9c5d4587e2fef9ba215e6ffeeb921f9ee95
RavitejaKarra24 <ravitejakarra24@gmail.com>
2024-10-07 09:15:13 +0530

    B: added typecraft lazy config

---
+++ typecraft_lazy/nvim/init.lua
@@ -0,0 +1,24 @@
+
+
+
+
+-- Lazy.nvim setup
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  print("Installing lazy.nvim...")
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+  vim.cmd("packadd lazy.nvim")
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("vim-options")
+require("lazy").setup("plugins")
+
